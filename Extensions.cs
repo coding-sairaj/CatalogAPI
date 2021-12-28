03ad4f8f69c23ef8679f0a7a5985813d331f8106
sairaj kondu <sairajkondu@sairajs-MacBook-Air.local>
2021-12-28 01:38:05 +0100

    Initial push restapi c#

---
+++ Extensions.cs
@@ -0,0 +1,16 @@
+using Catalog.Entities;
+using Catalog.DTOs;
+namespace Catalog;
+
+public static class Extensions
+{
+    public static ItemDto AsDto(this Item item)
+    {
+        return new ItemDto{
+            Id = item.Id,
+            Name = item.Name,
+            Price = item.Price,
+            CreatedDate = item.CreatedDate
+        };
+    }
+}
